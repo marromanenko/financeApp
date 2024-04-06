83ae2da6ab13b1e571d9121f757c83918697891c
Mariia <88029788+marromanenko@users.noreply.github.com>
2024-04-06 11:20:38 +0300

    Update main.js

--- frontend/frontend/src/main.js
+++ frontend/frontend/src/main.js
@@ -1,23 +1,56 @@
 import Vue from "vue";
-import './plugins/axios'
-import App from "./App.vue";
-import router from "./router";
-import vuetify from './plugins/vuetify'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-import VueSession from 'vue-session'
-import VueSweetalert2 from 'vue-sweetalert2';
-import store from './store'
+import VueRouter from "vue-router";
+import Auth from "../components/pages/Auth.vue"
+import Register from "../components/pages/Register.vue"
+import About from "../components/pages/About.vue"
+import Main from "../components/pages/Main.vue"
+import Profile from "../components/pages/Profile.vue"
+import Chat from "../components/pages/Chat.vue"
 
-Vue.use(Vuetify)
-Vue.use(VueSession)
-Vue.use(VueSweetalert2)
+Vue.use(VueRouter);
 
-Vue.config.productionTip = false;
+const routes = [
+  {
+    path: "/about",
+    name: "about",
+    component: About
+  },
+  {
+    path: "/login",
+    name: "auth",
+    component: Auth
+  },
+  {
+    path: "/register",
+    name: "register-page",
+    component: Register
+  },
+  {
+    path: "/main",
+    name: "main-page",
+    component: Main
+  },
+  {
+    path: "/profile",
+    name: "profile-page",
+    component: Profile
+  },
+  {
+    path: "/chat",
+    name: "chat-page",
+    component: Chat
+  }
+];
 
-new Vue({
-  router,
-  vuetify,
-  store,
-  render: (h) => h(App)
-}).$mount("#app");
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.name.charAt(0).toUpperCase() + to.name.slice(1);
+  next();
+});
+
+export default router;
